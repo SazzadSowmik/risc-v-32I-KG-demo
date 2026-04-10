2679ef830eecc7070c82170c0d0e7f4fe529d385
Sazzadul Islam <sazzadsowmik@Mac-934.lan>
2026-04-10 03:49:28 -0400

    fix import

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import KnowledgeGraph from "./KnowledgeGraph.jsx";
+import KnowledgeGraph from "./knowledgeGraph.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
